1355f459bd275e726396c56ecf4ab196e8b5b039
caltonji <caltonji@gmail.com>
2022-01-30 10:43:33 -0800

    first commit

---
+++ go.mod
@@ -0,0 +1,41 @@
+module search/rest-server
+
+go 1.17
+
+require (
+	cloud.google.com/go/firestore v1.6.1
+	github.com/gin-gonic/gin v1.7.7
+	github.com/google/uuid v1.1.2
+)
+
+require (
+	cloud.google.com/go v0.100.2 // indirect
+	cloud.google.com/go/compute v0.1.0 // indirect
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-playground/locales v0.13.0 // indirect
+	github.com/go-playground/universal-translator v0.17.0 // indirect
+	github.com/go-playground/validator/v10 v10.4.1 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.7 // indirect
+	github.com/googleapis/gax-go/v2 v2.1.1 // indirect
+	github.com/json-iterator/go v1.1.9 // indirect
+	github.com/leodido/go-urn v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
+	github.com/ugorji/go/codec v1.1.7 // indirect
+	go.opencensus.io v0.23.0 // indirect
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
+	golang.org/x/net v0.0.0-20210503060351-7fd8e65b6420 // indirect
+	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8 // indirect
+	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
+	golang.org/x/text v0.3.6 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	google.golang.org/api v0.66.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20220118154757-00ab72f36ad5 // indirect
+	google.golang.org/grpc v1.40.1 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/yaml.v2 v2.2.8 // indirect
+)
---
+++ main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"cloud.google.com/go/firestore"
+	"context"
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"log"
+	"net/http"
+	"fmt"
+	"google.golang.org/api/iterator"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.POST("/url", postUrl)
+	r.GET("/urls", getUrls)
+	// r.DELETE("/url", deleteUrl)
+	// r.GET("/search", getSearch)
+	r.Run()
+}
+
+func createClient(ctx context.Context) *firestore.Client {
+	// Sets your Google Cloud Platform project ID.
+	projectID := "precise-cabinet-280004"
+
+	client, err := firestore.NewClient(ctx, projectID)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+	// Close client when done with
+	// defer client.Close()
+	return client
+}
+
+// PostUrlBody is the body of the requests
+type PostUrlBody struct {
+	UserId string `json:"user_id" binding:"required"`
+	Url    string `json:"url" binding:"required"`
+}
+
+type Url struct {
+	Url string `json:"url" binding:"required"`
+	Id string `json:"id" binding:"required"`
+}
+
+func getUrls(c *gin.Context) {
+	// get the input
+	userId, ok := c.GetQuery("user_id")
+	if !ok {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "user_id is a required query param"})
+		return
+	}
+
+	// get the client
+	ctx := context.Background()
+	client := createClient(ctx)
+	defer client.Close()
+
+	fmt.Println(userId)
+
+	// get the urls
+	var urls []Url
+	iter := client.Collection("urls").Where("user_id", "==", userId).Documents(ctx)
+	for {
+		doc, err := iter.Next()
+		if err == iterator.Done {
+			fmt.Println("finished iterating")
+			break
+		}
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		data := doc.Data()
+		urls = append(urls, Url {
+			Url: data["user_id"].(string),
+			Id: data["id"].(string),
+		})
+	}
+	c.JSON(http.StatusOK, gin.H{"urls": urls})
+}
+
+func postUrl(c *gin.Context) {
+	// get the input
+	var postUrlBody PostUrlBody
+	if err := c.BindJSON(&postUrlBody); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// get the client
+	ctx := context.Background()
+	client := createClient(ctx)
+	defer client.Close()
+
+	// todo: scrape the website
+	// save the content
+	id := uuid.New().String()
+	_, err := client.Collection("urls").Doc(id).Set(ctx, map[string]interface{}{
+		"id":      id,
+		"user_id": postUrlBody.UserId,
+		"url":     postUrlBody.Url,
+	})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+}
